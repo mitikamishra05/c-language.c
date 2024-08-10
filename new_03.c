dfc04dab16c4e067ce494ee7c5ddd12a8b9d74c4
mitikamishra05 <155060558+mitikamishra05@users.noreply.github.com>
2024-08-10 22:19:32 +0530

    pull

---
+++ new_03.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a = 7;
+    int b = 8;
+    int c = a+b;
+    printf("the value of a is %d and the value of b is %d and sum is %d\n",a,b,c);
+    printf("the remainder when a is divided by b is: %d\n", a%b);
+    return 0;
+
+}
