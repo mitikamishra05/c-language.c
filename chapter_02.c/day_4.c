e95d7ccf9870aea2f012c1eff56893b086463894
mitikamishra05 <155060558+mitikamishra05@users.noreply.github.com>
2025-01-26 17:40:15 +0530

    pull

--- chapter_02.c/day_4.c
+++ chapter_02.c/day_4.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main(){
-    int length = 3
-    int breadth = 6
+    int length = 3;
+    int breadth = 6;
     printf("the area of a rectangle is %d", length*breadth);
     return 0;
 }
