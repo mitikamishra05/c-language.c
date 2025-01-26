31718bc0153a263fc2eb4e8928124e3d07d786e1
mitikamishra05 <155060558+mitikamishra05@users.noreply.github.com>
2025-01-26 17:30:52 +0530

    pull

---
+++ chapter_02.c/for_loop.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    for ( int i = 0; i < =15; i++)
+    {
+        if (i==10){
+            
+            continue;
+        }
+        printf("i is %d\n", i);
+        
+    }
+    printf("for loop is done!");
+
+    return 0;
+    
+}
